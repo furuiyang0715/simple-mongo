447e8a96d2b98f3219738a8ee5115f8df582558c
符瑞阳 <furuiyang@furuiyangdeMacBook-Pro.local>
2019-03-23 15:17:57 +0800

    little adjustment

--- tables.py
+++ tables.py
@@ -1,10 +1,10 @@
+"""排序后的数据库表"""
 tables = [
     'risk_data',
     'economic_gdp',
     'futures_basic',
     'comcn_embeddedvaluechange',
     'comcn_embeddedvalue',
-
     'comcn_embeddedvalueindex',
     'comcn_financespecialindexic',
     'economic_moneysupply',
@@ -12,7 +12,6 @@ tables = [
     'comcn_conceptlist',
     'const_personal',
     'comcn_bankindiconst',
-
     'comcn_financespecialindexsc',
     'hkland_historycashflow',
     'const_industry',
@@ -21,7 +20,6 @@ tables = [
     'hkland_historytradestat',
     'index_indexprepcomponent',
     'stk_codechangeserial',
-
     'const_secumain',
     'const_areacode',
     'const_newsconst',
@@ -30,17 +28,12 @@ tables = [
     'comcn_actualcontroller',
     'comcn_violationhalding',
     'const_product',
-
-
     'comcn_financespecialindex',
     'index_basicinfo',
     'comcn_msecufinance',
     'comcn_issuanceexamination',
     'stk_abbrchangeserial',
     'stk_secuchange',
-
-
-
     'const_industrytype',
     'const_ussecumain',
     'comcn_performanceletters',
@@ -50,58 +43,40 @@ tables = [
     'stk_business',
     'comcn_bankloan',
     'comcn_bankincomeexpense',
-
-
     'const_systemconst',
     'comcn_rewardstat',
     'derived_institution_detail',
-
-
     'comcn_coconcept',
     'news_secu',
     'comcn_bankregulator',
-
-
-
     'comcn_relatedsh',
     'comcn_exgindustry',
     'comcn_sharefpsta',
     'comcn_sharefp',
     'comcn_dividend',
-
     'comcn_managersstockalteration',
     'comcn_financialreportauditingopinion',
     'comcn_performanceforecast',
-
     'comcn_sharestru',
     'comcn_dividendprogress',
     'comcn_stockholdingst',
-
-
     'comcn_reservereportdate',
     'comcn_guaranteedetail',
     'stk_codechange',
-
     'const_tradingday',
     'const_hksecumain',
     'const_secumainall',
-
     'comcn_leaderposition',
     'stk_7percentchange',
     'stk_specialnotice',
-
-
     'comcn_qcashflowstatement',
     'comcn_qincomestatement',
     'comcn_qfinancialindex',
     'const_jydbdeleterec',
-
     'comcn_fsderiveddata',
     'comcn_fspecialindicators',
     'comcn_maindatanew',
     'comcn_cashflowstatementall_jy',
-
-
     'comcn_cashflowstatement',
     'comcn_balancesheetall_jy',
     'comcn_balancesheet',
@@ -111,7 +86,6 @@ tables = [
     'comcn_executivesholdings',
     'comcn_mainoperincome',
     'hkland_shares',
-
     'index_sywgindexquote',
     'index_quot_day',
     'comcn_equitychangesstatement',
@@ -128,108 +102,140 @@ tables = [
 ]
 
 
-o_tables = ['comcn_actualcontroller',
-'comcn_balancesheet',
-'comcn_balancesheetall_jy',
-'comcn_bankassetsliability',
-'comcn_bankincomeexpense',
-'comcn_bankindiconst',
-'comcn_bankloan',
-'comcn_bankregulator',
-'comcn_cashflowstatement',
-'comcn_cashflowstatementall_jy',
-'comcn_coconcept',
-'comcn_conceptlist',
-'comcn_controllingshareholders',
-'comcn_dindicesforvaluation',
-'comcn_dividend',
-'comcn_dividendprogress',
-'comcn_embeddedvalue',
-'comcn_embeddedvaluechange',
-'comcn_embeddedvalueindex',
-'comcn_equitychangesstatement',
-'comcn_executivesholdings',
-'comcn_exgindustry',
-'comcn_financespecialindex',
-'comcn_financespecialindexic',
-'comcn_financespecialindexsc',
-'comcn_financialreportauditingopinion',
-'comcn_fsderiveddata',
-'comcn_fspecialindicators',
-'comcn_guaranteedetail',
-'comcn_incomestatement',
-'comcn_incomestatementall_jy',
-'comcn_issuanceexamination',
-'comcn_leaderposition',
-'comcn_maindatanew',
-'comcn_mainoperincome',
-'comcn_mainquarterdata',
-'comcn_mainshlistnew',
-'comcn_managersstockalteration',
-'comcn_msecufinance',
-'comcn_nonrecurringevent',
-'comcn_performanceforecast',
-'comcn_performanceletters',
-'comcn_qcashflowstatement',
-'comcn_qfinancialindex',
-'comcn_qincomestatement',
-'comcn_relatedsh',
-'comcn_reservereportdate',
-'comcn_rewardstat',
-'comcn_sharefp',
-'comcn_sharefpsta',
-'comcn_sharesfloatingschedule',
-'comcn_sharestru',
-'comcn_stockholdingst',
-'comcn_violationhalding',
-'const_areacode',
-'const_hksecumain',
-'const_industry',
-'const_industrytype',
-'const_jydbdeleterec',
-'const_keywords',
-'const_newsconst',
-'const_personal',
-'const_product',
-'const_secumain',
-'const_secumainall',
-'const_systemconst',
-'const_tradingday',
-'const_ussecumain',
-'derived_institution_detail',
-'derived_institution_summary',
-'economic_gdp',
-'economic_moneysupply',
-'futures_basic',
-'hkland_historycashflow',
-'hkland_historytradestat',
-'hkland_shares',
-'index_basicinfo',
-'index_indexcomponentsweight',
-'index_indexprepcomponent',
-'index_quot_day',
-'index_swsindexcw',
-'index_sywgindexquote',
-'index_weight',
-'news_secu',
-'risk_data',
-'stk_7percentchange',
-'stk_abbrchangeserial',
-'stk_business',
-'stk_codechange',
-'stk_codechangeserial',
-'stk_liststatus',
-'stk_quot_day',
-'stk_quot_idx',
-'stk_quotidxwind_day',
-'stk_quotori_day',
-'stk_secuchange',
-'stk_specialnotice',
-'stk_specialtrade',
-'trans_valuations']
+"""排序前的原始数据库表"""
+o_tables = [
+    'comcn_actualcontroller',
+    'comcn_balancesheet',
+    'comcn_balancesheetall_jy',
+    'comcn_bankassetsliability',
+    'comcn_bankincomeexpense',
+    'comcn_bankindiconst',
+    'comcn_bankloan',
+    'comcn_bankregulator',
+    'comcn_cashflowstatement',
+    'comcn_cashflowstatementall_jy',
+    'comcn_coconcept',
+    'comcn_conceptlist',
+    'comcn_controllingshareholders',
+    'comcn_dindicesforvaluation',
+    'comcn_dividend',
+    'comcn_dividendprogress',
+    'comcn_embeddedvalue',
+    'comcn_embeddedvaluechange',
+    'comcn_embeddedvalueindex',
+    'comcn_equitychangesstatement',
+    'comcn_executivesholdings',
+    'comcn_exgindustry',
+    'comcn_financespecialindex',
+    'comcn_financespecialindexic',
+    'comcn_financespecialindexsc',
+    'comcn_financialreportauditingopinion',
+    'comcn_fsderiveddata',
+    'comcn_fspecialindicators',
+    'comcn_guaranteedetail',
+    'comcn_incomestatement',
+    'comcn_incomestatementall_jy',
+    'comcn_issuanceexamination',
+    'comcn_leaderposition',
+    'comcn_maindatanew',
+    'comcn_mainoperincome',
+    'comcn_mainquarterdata',
+    'comcn_mainshlistnew',
+    'comcn_managersstockalteration',
+    'comcn_msecufinance',
+    'comcn_nonrecurringevent',
+    'comcn_performanceforecast',
+    'comcn_performanceletters',
+    'comcn_qcashflowstatement',
+    'comcn_qfinancialindex',
+    'comcn_qincomestatement',
+    'comcn_relatedsh',
+    'comcn_reservereportdate',
+    'comcn_rewardstat',
+    'comcn_sharefp',
+    'comcn_sharefpsta',
+    'comcn_sharesfloatingschedule',
+    'comcn_sharestru',
+    'comcn_stockholdingst',
+    'comcn_violationhalding',
+    'const_areacode',
+    'const_hksecumain',
+    'const_industry',
+    'const_industrytype',
+    'const_jydbdeleterec',
+    'const_keywords',
+    'const_newsconst',
+    'const_personal',
+    'const_product',
+    'const_secumain',
+    'const_secumainall',
+    'const_systemconst',
+    'const_tradingday',
+    'const_ussecumain',
+    'derived_institution_detail',
+    'derived_institution_summary',
+    'economic_gdp',
+    'economic_moneysupply',
+    'futures_basic',
+    'hkland_historycashflow',
+    'hkland_historytradestat',
+    'hkland_shares',
+    'index_basicinfo',
+    'index_indexcomponentsweight',
+    'index_indexprepcomponent',
+    'index_quot_day',
+    'index_swsindexcw',
+    'index_sywgindexquote',
+    'index_weight',
+    'news_secu',
+    'risk_data',
+    'stk_7percentchange',
+    'stk_abbrchangeserial',
+    'stk_business',
+    'stk_codechange',
+    'stk_codechangeserial',
+    'stk_liststatus',
+    'stk_quot_day',
+    'stk_quot_idx',
+    'stk_quotidxwind_day',
+    'stk_quotori_day',
+    'stk_secuchange',
+    'stk_specialnotice',
+    'stk_specialtrade',
+    'trans_valuations'
+]
 
 
-# dd = {'comcn_actualcontroller': 4754, 'comcn_balancesheet': 603318, 'comcn_balancesheetall_jy': 603291, 'comcn_bankassetsliability': 20055, 'comcn_bankincomeexpense': 28283, 'comcn_bankindiconst': 736, 'comcn_bankloan': 24028, 'comcn_bankregulator': 54127, 'comcn_cashflowstatement': 548872, 'comcn_cashflowstatementall_jy': 548863, 'comcn_coconcept': 40361, 'comcn_conceptlist': 571, 'comcn_controllingshareholders': 4518, 'comcn_dindicesforvaluation': 9999341, 'comcn_dividend': 80447, 'comcn_dividendprogress': 144433, 'comcn_embeddedvalue': 87, 'comcn_embeddedvaluechange': 68, 'comcn_embeddedvalueindex': 87, 'comcn_equitychangesstatement': 2694503, 'comcn_executivesholdings': 1145354, 'comcn_exgindustry': 60926, 'comcn_financespecialindex': 5690, 'comcn_financespecialindexic': 97, 'comcn_financespecialindexsc': 789, 'comcn_financialreportauditingopinion': 98355, 'comcn_fsderiveddata': 313113, 'comcn_fspecialindicators': 322864, 'comcn_guaranteedetail': 168727, 'comcn_incomestatement': 756349, 'comcn_incomestatementall_jy': 756351, 'comcn_issuanceexamination': 8500, 'comcn_leaderposition': 235854, 'comcn_maindatanew': 347181, 'comcn_mainoperincome': 1197039, 'comcn_mainquarterdata': 22597, 'comcn_mainshlistnew': 2852981, 'comcn_managersstockalteration': 89824, 'comcn_msecufinance': 8276, 'comcn_nonrecurringevent': 1032295, 'comcn_performanceforecast': 100574, 'comcn_performanceletters': 17255, 'comcn_qcashflowstatement': 249527, 'comcn_qfinancialindex': 278147, 'comcn_qincomestatement': 277968, 'comcn_relatedsh': 55268, 'comcn_reservereportdate': 165353, 'comcn_rewardstat': 38840, 'comcn_sharefp': 79521, 'comcn_sharefpsta': 77773, 'comcn_sharesfloatingschedule': 21496, 'comcn_sharestru': 143541, 'comcn_stockholdingst': 146688, 'comcn_violationhalding': 4784, 'const_areacode': 3910, 'const_hksecumain': 205239, 'const_industry': 1345, 'const_industrytype': 10294, 'const_jydbdeleterec': 296799, 'const_keywords': 448, 'const_newsconst': 4042, 'const_personal': 581, 'const_product': 5297, 'const_secumain': 3711, 'const_secumainall': 212492, 'const_systemconst': 36110, 'const_tradingday': 184313, 'const_ussecumain': 12736, 'derived_institution_detail': 39996, 'derived_institution_summary': 1958, 'economic_gdp': 52, 'economic_moneysupply': 134, 'futures_basic': 57, 'hkland_historycashflow': 1000, 'hkland_historytradestat': 3055, 'hkland_shares': 1258542, 'index_basicinfo': 7992, 'index_indexcomponentsweight': 14179830, 'index_indexprepcomponent': 3224, 'index_quot_day': 2036860, 'index_swsindexcw': 48099256, 'index_sywgindexquote': 1762138, 'index_weight': 29288935, 'news_secu': 43290, 'risk_data': 0, 'stk_7percentchange': 241129, 'stk_abbrchangeserial': 9756, 'stk_business': 22603, 'stk_codechange': 182271, 'stk_codechangeserial': 3701, 'stk_liststatus': 4099, 'stk_quot_day': 10268521, 'stk_quot_idx': 10268521, 'stk_quotidxwind_day': 9694422, 'stk_quotori_day': 9505973, 'stk_secuchange': 9840, 'stk_specialnotice': 244273, 'stk_specialtrade': 2073, 'trans_valuations': 2937607}
+# dd = {'comcn_actualcontroller': 4754, 'comcn_balancesheet': 603318, 'comcn_balancesheetall_jy': 603291,
+# 'comcn_bankassetsliability': 20055, 'comcn_bankincomeexpense': 28283, 'comcn_bankindiconst': 736,
+# 'comcn_bankloan': 24028, 'comcn_bankregulator': 54127, 'comcn_cashflowstatement': 548872,
+# 'comcn_cashflowstatementall_jy': 548863, 'comcn_coconcept': 40361, 'comcn_conceptlist': 571,
+# 'comcn_controllingshareholders': 4518, 'comcn_dindicesforvaluation': 9999341, 'comcn_dividend': 80447,
+# 'comcn_dividendprogress': 144433, 'comcn_embeddedvalue': 87, 'comcn_embeddedvaluechange': 68,
+# 'comcn_embeddedvalueindex': 87, 'comcn_equitychangesstatement': 2694503, 'comcn_executivesholdings': 1145354,
+# 'comcn_exgindustry': 60926, 'comcn_financespecialindex': 5690, 'comcn_financespecialindexic': 97,
+# 'comcn_financespecialindexsc': 789, 'comcn_financialreportauditingopinion': 98355, 'comcn_fsderiveddata': 313113,
+# 'comcn_fspecialindicators': 322864, 'comcn_guaranteedetail': 168727, 'comcn_incomestatement': 756349,
+# 'comcn_incomestatementall_jy': 756351, 'comcn_issuanceexamination': 8500, 'comcn_leaderposition': 235854,
+# 'comcn_maindatanew': 347181, 'comcn_mainoperincome': 1197039, 'comcn_mainquarterdata': 22597,
+# 'comcn_mainshlistnew': 2852981, 'comcn_managersstockalteration': 89824, 'comcn_msecufinance': 8276,
+# 'comcn_nonrecurringevent': 1032295, 'comcn_performanceforecast': 100574, 'comcn_performanceletters': 17255,
+# 'comcn_qcashflowstatement': 249527, 'comcn_qfinancialindex': 278147, 'comcn_qincomestatement': 277968,
+# 'comcn_relatedsh': 55268, 'comcn_reservereportdate': 165353, 'comcn_rewardstat': 38840, 'comcn_sharefp': 79521,
+# 'comcn_sharefpsta': 77773, 'comcn_sharesfloatingschedule': 21496, 'comcn_sharestru': 143541,
+# 'comcn_stockholdingst': 146688, 'comcn_violationhalding': 4784, 'const_areacode': 3910, 'const_hksecumain': 205239,
+# 'const_industry': 1345, 'const_industrytype': 10294, 'const_jydbdeleterec': 296799, 'const_keywords': 448,
+# 'const_newsconst': 4042, 'const_personal': 581, 'const_product': 5297, 'const_secumain': 3711,
+# 'const_secumainall': 212492, 'const_systemconst': 36110, 'const_tradingday': 184313, 'const_ussecumain': 12736,
+# 'derived_institution_detail': 39996, 'derived_institution_summary': 1958, 'economic_gdp': 52,
+# 'economic_moneysupply': 134, 'futures_basic': 57, 'hkland_historycashflow': 1000, 'hkland_historytradestat': 3055,
+# 'hkland_shares': 1258542, 'index_basicinfo': 7992, 'index_indexcomponentsweight': 14179830, '
+# index_indexprepcomponent': 3224, 'index_quot_day': 2036860, 'index_swsindexcw': 48099256,
+# 'index_sywgindexquote': 1762138, 'index_weight': 29288935, 'news_secu': 43290, 'risk_data': 0,
+# 'stk_7percentchange': 241129, 'stk_abbrchangeserial': 9756, 'stk_business': 22603, 'stk_codechange': 182271,
+# 'stk_codechangeserial': 3701, 'stk_liststatus': 4099, 'stk_quot_day': 10268521, 'stk_quot_idx': 10268521,
+# 'stk_quotidxwind_day': 9694422, 'stk_quotori_day': 9505973, 'stk_secuchange': 9840, 'stk_specialnotice': 244273,
+# 'stk_specialtrade': 2073, 'trans_valuations': 2937607}
 #
 # dd = sorted(dd.items(), key=lambda item: item[1])
 #
@@ -237,33 +243,4 @@ o_tables = ['comcn_actualcontroller',
 #
 # res = [d[0] for d in dd]
 #
-# print(res)
-
-
-def td_format(td_object):
-    seconds = int(td_object.total_seconds())
-    # print(seconds)
-    return seconds
-    # periods = [
-    #     ('year',        60*60*24*365),
-    #     ('month',       60*60*24*30),
-    #     ('day',         60*60*24),
-    #     ('hour',        60*60),
-    #     ('minute',      60),
-    #     ('second',      1)
-    # ]
-    #
-    # strings = []
-    # for period_name, period_seconds in periods:
-    #     if seconds > period_seconds:
-    #         period_value , seconds = divmod(seconds, period_seconds)
-    #         has_s = 's' if period_value > 1 else ''
-    #         strings.append("%s %s%s" % (period_value, period_name, has_s))
-    #
-    # return ", ".join(strings)
-
-
-# import datetime
-# demo_dt = datetime.timedelta(seconds=76500)
-# res = td_format(demo_dt)
 # print(res)
