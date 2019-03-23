c785ac65f032d11ee666057b9f2f7ea1e84eb05e
符瑞阳 <furuiyang@furuiyangdeMacBook-Pro.local>
2019-03-23 14:20:15 +0800

    抽样的基础代码结构

--- random_check.py
+++ random_check.py
@@ -1,4 +1,38 @@
-# 针对每个数据库进行随机抽样检查
+# 抽样
+
+import configparser
+import logging
+
+from common import SyncData
+from mongodb import MyMongoDB
+
+config = configparser.ConfigParser()
+config.read('./conf/config.ini')
+
+logging.config.fileConfig('conf/logging.conf')
+logger = logging.getLogger('root')
+
+mongo = MyMongoDB(config)
+run_demo = SyncData(mongo)
+
+
+def each_check(table):
+    pass
+
+    return True
+
+
+def main_check(tables):
+    suc_tables = list()
+    fail_tables = list()
+    try:
+        for table in tables:
+            suc_tables.append(table) if each_check(table) else fail_tables.append(table)
+    except Exception as e:
+        logger.warning(f'some thing have been wrong during the check: {e}')
+        raise
+    logger.info(f"The ret of check is success: {suc_tables}, fail: {fail_tables}")
+    return
 
 
 
