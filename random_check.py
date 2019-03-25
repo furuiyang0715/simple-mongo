fba6558ae9ba075af6fd79487665e67dbaba52c2
符瑞阳 <furuiyang@furuiyangdeMacBook-Pro.local>
2019-03-25 10:20:08 +0800

    简单随机抽样和系统抽样

--- random_check.py
+++ random_check.py
@@ -1,7 +1,8 @@
-# 抽样
+# 抽样 写成一个单脚本的模式即可
 
 import configparser
 import logging
+import random
 
 from common import SyncData
 from mongodb import MyMongoDB
@@ -17,6 +18,7 @@ run_demo = SyncData(mongo)
 
 
 def each_check(table):
+
     pass
 
     return True
@@ -35,8 +37,27 @@ def main_check(tables):
     return
 
 
+def SystematicSampling(table, group):
+    try:
+        coll = mongo.get_coll(table, "datacenter")
+        total = mongo.gen_count(coll)
+    except Exception as e:
+        logger.warning(f"系统抽样中计算总数失败， {e}")
+        raise SystemError(e)
+    sub_num = round(total/group)  # round(101/5) == 20
+    samples = list()
+    for i in range(group):
+        samples.append(RandomSampling(total, sub_num, i))
+    return samples
 
 
-
-
-
+def RandomSampling(total, sub_num, i):
+    start = i * sub_num   # 0, 1, 2, 3, 4    # 0, 20, 40, 60, 80
+    end = (i+1) * sub_num - 1                # 20, 40, 60, 80, 100
+    # 0-19 20-39 40-59 60-79 80-99
+    try:
+        sample = random.randint(start, end)
+        return sample
+    except Exception as e:
+        logger.warning(f'简单随机抽样过程失败， {e}')
+        raise SystemError(e)
